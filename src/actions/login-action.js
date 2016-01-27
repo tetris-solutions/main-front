2f945dee1defb1e3f417c6c283249425d2cdf354
Christian Amaral <darthcas@gmail.com>
2016-01-27 18:58:10 -0200

    atualizado versão user-api ajuste no login

--- src/actions/login-action.js
+++ src/actions/login-action.js
@@ -1,11 +1,10 @@
 import login from '../api/login'
-import loadUser from '../api/load-user'
 import persistTokenAsCookies from '../functions/persist-token-as-cookie'
 
 export default (tree, email, password) =>
   login(email, password)
     .then(persistTokenAsCookies)
-    .then(() => loadUser())
-    .then(user => {
-      tree.set('user', user)
+    .then(response => {
+      tree.set('user', response.data)
+      tree.commit()
     })
