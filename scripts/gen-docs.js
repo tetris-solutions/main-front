38e03b3fdf9293620b96792eb73325152d60406e
Christian Amaral <darthcas@gmail.com>
2016-02-19 17:54:23 -0200

    docs

---
+++ scripts/gen-docs.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+var $ = require('shelljs')
+var fs = require('fs')
+
+$.mv('.babelrc', '_babelrc')
+$.rm('-rf', '.tmp')
+$.mkdir('-p', '.tmp')
+$.cd('.tmp')
+
+var pkg = require('../package.json')
+
+$.cp('../package.json', '.')
+
+$.ln('-s', '../src', 'src')
+$.ln('-s', '../docs', 'docs')
+$.ln('-s', '../node_modules', 'node_modules')
+$.ln('-s', '../.git', '.git')
+
+const entries = ['src/server.js'/*, 'src/client.js'*/]
+
+entries.forEach(entry => {
+  pkg.main = entry
+  fs.writeFileSync('package.json', JSON.stringify(pkg, null, 2))
+  $.exec('node_modules/.bin/documentation build --github -f html -o docs')
+})
+
+$.cd('..')
+$.rm('-rf', '.tmp')
+$.mv('_babelrc', '.babelrc')
--- scripts/gen-docs.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-mv .babelrc _babelrc
-node_modules/.bin/documentation build --github -f html -o docs
-mv _babelrc .babelrc
