e2597aae15ae78d9907f7d76f3065c5abb4b8201
Christian Amaral <darthcas@gmail.com>
2016-02-19 11:17:50 -0200

    docs login-action

--- src/actions/login-action.js
+++ src/actions/login-action.js
@@ -1,11 +1,21 @@
 import login from '../api/login'
-import persistTokenAsCookies from '../functions/persist-token-as-cookie'
+import persistTokenAsCookie from '../functions/persist-token-as-cookie'
 import getApiFetchConfig from '../functions/get-api-fetch-config'
 
-export default (tree, email, password) =>
-  login(email, password, getApiFetchConfig(tree))
-    .then(persistTokenAsCookies)
+/**
+ * fires request to login API and save user object in the state tree on success
+ * @param {Baobab} tree state tree
+ * @param {string} email user email
+ * @param {string} password user password
+ * @returns {Promise} promise that resolves once action is complete
+ */
+function loginAction (tree, email, password) {
+  return login(email, password, getApiFetchConfig(tree))
+    .then(persistTokenAsCookie)
     .then(response => {
       tree.set('user', response.data)
       tree.commit()
     })
+}
+
+export default loginAction
