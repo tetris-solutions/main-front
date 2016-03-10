9062ff77649fbfc9e5f609876449e330a48d3b1b
Christian Amaral <darthcas@gmail.com>
2016-03-10 13:29:34 -0300

    disable localStorage cache for the time being

--- src/client-tree.js
+++ src/client-tree.js
@@ -1,16 +1,16 @@
 import Tree from 'baobab'
-import loadCachedState from './functions/load-cached-state'
-import cacheState from './functions/cache-state'
+// import loadCachedState from './functions/load-cached-state'
+// import cacheState from './functions/cache-state'
 import merge from 'lodash/merge'
 import window from 'global/window'
 import defaultState from './default-state'
 import Cookies from 'js-cookie'
 
-const tree = new Tree(merge(defaultState, loadCachedState(), window.backendPayload))
+const tree = new Tree(merge(defaultState, /* loadCachedState(), */window.backendPayload))
 
-tree
-  .select('user')
-  .on('update', () => cacheState(tree.get()))
+// tree
+//   .select('user')
+//   .on('update', () => cacheState(tree.get()))
 
 tree.select('locale').on('update', ({data: {currentData}}) => {
   Cookies.set(process.env.LOCALE_COOKIE_NAME, currentData, {
