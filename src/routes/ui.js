7cf97d86089ed6ae41c5c9c204cc87b97a56521e
Christian Amaral <darthcas@gmail.com>
2016-10-19 19:16:55 -0200

    move login to a higher route

--- src/routes/ui.js
+++ src/routes/ui.js
@@ -9,16 +9,30 @@ import {root} from 'baobab-react/higher-order'
 import {dashboardRoutes} from './ui-dashboard'
 import Header from '../components/Header'
 
+const Main = ({children}) => (
+  <div>
+    <Header/>
+    {children}
+  </div>
+)
+Main.displayName = 'Main'
+Main.propTypes = {
+  children: React.PropTypes.node.isRequired
+}
+
 export function getRoutes (tree, protectRoute, preload, createRoot) {
   return (
-    <Route path='/' component={root(tree, createRoot(Header))}>
-      <IndexRoute component={Home}/>
+    <Route path='/' component={root(tree, createRoot())}>
       <Route path='login' component={Login}/>
-      <Route path='signup' component={Signup}/>
-      <Route path='waiting-confirmation' component={WaitingConfirmation}/>
-      <Route path='activate/:activationCode' component={Activation}/>
-      <Route onEnter={protectRoute}>
-        {dashboardRoutes(preload)}
+
+      <Route component={Main}>
+        <IndexRoute component={Home}/>
+        <Route path='signup' component={Signup}/>
+        <Route path='waiting-confirmation' component={WaitingConfirmation}/>
+        <Route path='activate/:activationCode' component={Activation}/>
+        <Route onEnter={protectRoute}>
+          {dashboardRoutes(preload)}
+        </Route>
       </Route>
     </Route>
   )
