7105f0599194c1a6885e9c014a9d466aaa3a31ac
Christian Amaral <darthcas@gmail.com>
2017-06-02 10:27:43 -0300

    load accounts on extended mode

--- src/api/load-account.js
+++ src/api/load-account.js
@@ -1,5 +1,5 @@
 import {GET} from '@tetris/http'
 
 export function loadAccount (id, config) {
-  return GET(`${process.env.TKM_URL}/account/${id}`, config)
+  return GET(`${process.env.TKM_URL}/account/${id}?extended=true`, config)
 }
